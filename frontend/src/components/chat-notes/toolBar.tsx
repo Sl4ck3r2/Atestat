3eb8b4e1507c7cbecdb8680919fa5f5063810201
Sl4ck3r2 <52386237+Sl4ck3r2@users.noreply.github.com>
2023-03-08 09:15:25 +0200

    fix: avatar insted of button

--- frontend/src/components/chat-notes/toolBar.tsx
+++ frontend/src/components/chat-notes/toolBar.tsx
@@ -1,5 +1,5 @@
 import { MessageOutlined, TeamOutlined, UsergroupAddOutlined } from '@ant-design/icons';
-import { Avatar, Badge, Button, Space } from 'antd';
+import { Badge, Button } from 'antd';
 import { FC } from 'react';
 
 import styles from './index.module.scss';
@@ -11,34 +11,34 @@ interface ToolBarProps {
 const ToolBar: FC<ToolBarProps> = ({ handleWindow }) => {
   return (
     <div className={styles.toolBarContainer}>
-      <Badge color="#AA14F0" offset={[-25, 8]} count={5}>
-        <Avatar
-          onClick={() => handleWindow(WINDOW.conversation)}
-          style={{ color: '#AA14F0', backgroundColor: '#EEEEEE' }}
-          icon={<MessageOutlined />}
-          shape="square"
+      <Badge color="#AA14F0" offset={[-27, 10]} count={5}>
+        <Button
+          style={{ fontSize: '1.5rem', color: '#AA14F0' }}
           size="large"
-        />
+          onClick={() => handleWindow(WINDOW.conversation)}
+          type="text"
+          icon={<MessageOutlined />}
+        ></Button>
         <div className={styles.toolName}>Conversation</div>
       </Badge>
-      <Badge color="#AA14F0" offset={[-10, 8]} count={3}>
-        <Avatar
-          onClick={() => handleWindow(WINDOW.friends)}
-          style={{ color: '#AA14F0', backgroundColor: '#EEEEEE' }}
-          icon={<TeamOutlined />}
-          shape="square"
+      <Badge color="#AA14F0" offset={[-11, 12]} count={3}>
+        <Button
+          style={{ fontSize: '1.5rem', color: '#AA14F0' }}
           size="large"
-        />
+          onClick={() => handleWindow(WINDOW.friends)}
+          type="text"
+          icon={<TeamOutlined />}
+        ></Button>
         <div className={styles.toolName}>Friends</div>
       </Badge>
       <Badge>
-        <Avatar
-          onClick={() => handleWindow(WINDOW.addFriends)}
-          style={{ color: '#AA14F0', backgroundColor: '#EEEEEE' }}
-          icon={<UsergroupAddOutlined />}
-          shape="square"
+        <Button
+          style={{ fontSize: '1.5rem', color: '#AA14F0' }}
           size="large"
-        />
+          onClick={() => handleWindow(WINDOW.addFriends)}
+          type="text"
+          icon={<UsergroupAddOutlined />}
+        ></Button>
         <div className={styles.toolName}>Add Friends</div>
       </Badge>
     </div>
